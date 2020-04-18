70a71c9f67b6973fe2cad189bf974ea5b306ece5
hideo54 <hideo54@hideo54.com>
2020-04-18 09:07:00 +0900

    Post to Slack

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import scrapeIt from 'scrape-it';
 import schedule from 'node-schedule';
+import { WebClient } from '@slack/web-api';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const slackChannel = process.env.SLACK_CHANNEL!;
+const slackToken = process.env.SLACK_BOT_TOKEN;
+const webClient = new WebClient(slackToken);
 
 const url = 'https://www.amazon.co.jp/dp/B07XV8VSZT/'; // Ring Fit
+// const url = 'https://www.amazon.co.jp/dp/B07V3KK93X/'; // Pokemon (for test)
 
 interface AmazonStockData {
     title: string;
@@ -13,12 +21,22 @@ const checkStock = async (url: string) => {
         title: 'span#productTitle',
         buyboxTextContent: 'div#buybox',
     });
-    return data.buyboxTextContent.includes('カートに入れる');
+    return {
+        title: data.title,
+        isInStock: data.buyboxTextContent.includes('カートに入れる')
+    };
 };
 
 const job = async () => {
-    const isInStock = await checkStock(url);
-    console.log(isInStock);
+    const { title, isInStock } = await checkStock(url);
+    if (isInStock) {
+        await webClient.chat.postMessage({
+            channel: slackChannel,
+            text: `Amazon に *${title}* が入荷したよ〜! 今すぐ購入!! ${url}`,
+            icon_emoji: ':amazon:',
+            username: 'Amazon入荷情報',
+        });
+    }
 };
 
 if (process.env.NODE_ENV === 'develop') {
